8a428f7dfcdbaaee9a949d188a1b64de0327ea0f
allefgoncalves <allefgoncalves@gmail.com>
2025-06-29 17:56:10 -0300

    criação da mutiplicação

---
+++ main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+class Operacoes{
+    private:
+        double operador1; //Primeiro Operador
+        double operador2; //Segundo Operador
+    public:
+        Operacoes(double Op1, double Op2){
+            operador1 = Op1;
+            operador2 = Op2;
+        }
+        
+        double multiplicar(){
+            return operador1 * operador2; 
+        }
+
+    
+};
+
+int main(){
+    Operacoes operacao(3, 2);
+    cout << "resultado da multiplicao: " << operacao.multiplicar() << endl;
+    return 0;
+}
